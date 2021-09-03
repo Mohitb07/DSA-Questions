e61670dae138dfa4f7221c664806e703fbc19b11
mohitb07 <bmohit980@gmail.com>
2021-09-03 21:04:26 +0530

    Pivot index problem

---
+++ Arrays/pivot.js
@@ -0,0 +1,29 @@
+// Find Pivot Index
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+const pivotIndex = function(nums){
+    let totalSum = 0;
+    let leftSum = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        totalSum+=nums[i]
+    }
+
+    for(let i = 0; i < nums.length; i++){
+        if(leftSum === totalSum - nums[i] - leftSum){
+           return i; 
+        }
+        leftSum+=nums[i]
+    }
+
+    return -1;
+}
+
+console.log(pivotIndex([1,7,3,6,5,6]))
