9b1fd3f632a9389e7aaf23d9cb395b0ff44507b1
mohitb07 <bmohit980@gmail.com>
2021-09-06 21:04:27 +0530

    Optimized solution for Finding the duplicate in an array

--- Arrays/Duplicate.js
+++ Arrays/Duplicate.js
@@ -9,21 +9,41 @@ function duplicate(arr){
         }
     }
 }
+// console.log(duplicate([1,3,4,2,2]))
+// 2) SECOND APPROACH (Working on it!)
+function duplicate2(arr){
+    const newArr = new Array(arr.length)
+    for(let i = 0; i <= arr.length; i++){      
+        console.log('check', arr[i])
+        if(newArr[arr[i]] !== arr[i]){
+            console.log('!', arr[i])
+            newArr[arr[i]] = 1
+        }else {
+            console.log('includes', arr[i])
+            newArr[arr[i]] = newArr[arr[i]] + 1;
+        }
+    }
+    console.table(newArr)
+}
 
-console.log(duplicate([1,3,4,2,2]))
-// function duplicate2(arr){
-//     const newArr = new Array(arr.length)
-//     for(let i = 0; i <= arr.length; i++){      
-//         console.log('check', arr[i])
-//         if(newArr[arr[i]] !== arr[i]){
-//             console.log('!', arr[i])
-//             newArr[arr[i]] = 1
-//         }else {
-//             console.log('includes', arr[i])
-//             newArr[arr[i]] = newArr[arr[i]] + 1;
-//         }
-//     }
-//     console.table(newArr)
-// }
+console.log(duplicate2([3,1,3,4,2]))
 
-// console.log(duplicate2([3,1,3,4,2]))
+// 3) THIRD APPROACH Linked List Cycle Approach
+function duplicate3(arr){
+    var slow = arr[0]
+    var fast = arr[0]
+
+    do{
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+    }while(slow !== fast)
+
+    fast = arr[0]
+    while(slow != fast){
+        slow = arr[slow]
+        fast = arr[fast]
+    }
+    return slow;
+}
+
+console.log(duplicate3([3,1,3,4,2]))
