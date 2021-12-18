3a0c5bb516cc9201d50f855df9f69bfeb0c75dc2
mohitb07 <bmohit980@gmail.com>
2021-12-18 21:43:46 +0530

    integer to roman problem added

---
+++ intToRoman.js
@@ -0,0 +1,19 @@
+function intToRoman(num) {
+  var map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  var result = "";
+  for (var i = 0; i < map.length; i++) {
+    while (num >= map[Object.keys(map)[i]]) {
+      num -= map[Object.keys(map)[i]];
+      result += Object.keys(map)[i];
+    }
+  }
+  return result;
+}
