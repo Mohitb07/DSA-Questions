667ecc40bf7ede16589af9230ce01ba10f2aa360
Mohitb07 <bmohit980@gmail.com>
2022-11-29 21:48:02 +0530

    bug fixed

--- Stack/largestRectangularArea.js
+++ Stack/largestRectangularArea.js
@@ -1,12 +1,10 @@
 const Stack = require('./Stack')
 
 const nextSmallest = (arr, size) => {
-    console.log('called nextSmallest')
     const s1 = new Stack()
     s1.push(-1)
     const result = []
     for(let i = size - 1; i >= 0; i--){
-        console.log('show stack', i, s1.showStack())
         let curr = arr[i]
         while(s1.peek() !== -1 && arr[s1.peek()] >= curr){
             s1.pop()
@@ -17,12 +15,10 @@ const nextSmallest = (arr, size) => {
     return result;
 }
 const prevSmallest = (arr, size) => {
-    console.log('called prevSmallest')
     const s1 = new Stack()
     s1.push(-1)
     const result = []
     for(let i = 0; i < size; i++){
-        console.log('curr', i)
         let curr = arr[i]
         while(s1.peek() !== -1 && arr[s1.peek()] >= curr){
             s1.pop()
@@ -34,7 +30,6 @@ const prevSmallest = (arr, size) => {
 }
 
 const largestRectangularArea = (arr, size) => {
-    console.log('called')
     let next, prev, area = 0;
     next = nextSmallest(arr, size)
     prev = prevSmallest(arr, size)
@@ -42,10 +37,10 @@ const largestRectangularArea = (arr, size) => {
     console.log('prev', prev)
     for(let i = 0; i < size; i++){
         let length = arr[i]
-        let breadth = next[i] - prev[i] - 1
         if(next[i] === -1){
             next[i] = size
         }        
+        let breadth = next[i] - prev[i] - 1
         let newArea = length*breadth
         area = Math.max(area, newArea)
     }
@@ -53,5 +48,5 @@ const largestRectangularArea = (arr, size) => {
 }
 
 
-const heights = [2,1,5,6,2,3]
+const heights = [2,4]
 console.log('area', largestRectangularArea(heights, heights.length))
