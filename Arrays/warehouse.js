cb042fe689e42531b5b95943dac35d114acf0653
Mohitb07 <bmohit980@gmail.com>
2023-02-15 21:31:27 +0530

    refactor: changed approach

--- Arrays/warehouse.js
+++ Arrays/warehouse.js
@@ -1,17 +1,14 @@
 // itemId is ID of item
 
 function warehouse(itemId, warehouseItems) {
-    if(warehouseItems.includes(itemId)){
-        return warehouseItems.indexOf(itemId)
-    }else {
-        warehouseItems.push(itemId)
-        const latestWarehouse = warehouseItems.sort((a,b ) => a - b) 
-        return latestWarehouse.indexOf(itemId)
-    }
+  if (warehouseItems.indexOf(itemId) !== -1)
+    return warehouseItems.indexOf(itemId);
+  warehouseItems.push(itemId);
+  const latestWarehouse = warehouseItems.sort((a, b) => a - b);
+  return latestWarehouse.indexOf(itemId);
 }
 
-const warehouseItems = [2, 3, 4 , 10, 11, 23, 34, 45, 56]
-const newItemId = 35
+const warehouseItems = [2, 3, 4, 10, 11, 23, 34, 45, 56];
+const newItemId = 35;
 
-
-console.log(warehouse(newItemId, warehouseItems))
+console.log(warehouse(newItemId, warehouseItems));
